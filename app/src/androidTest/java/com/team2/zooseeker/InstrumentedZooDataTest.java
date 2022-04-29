889a42bf9eb2a4c6f3a5844dd85807bffc29709b
Dylan Barva <d.barva5402@gmail.com>
2022-04-29 16:02:29 -0700

    add tests for loading zoo data

---
+++ app/src/androidTest/java/com/team2/zooseeker/InstrumentedZooDataTest.java
@@ -0,0 +1,36 @@
+package com.team2.zooseeker;
+
+import static org.junit.Assert.assertEquals;
+
+import android.content.Context;
+
+import androidx.test.core.app.ApplicationProvider;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Map;
+
+import cse110.ZooData;
+
+@RunWith(AndroidJUnit4.class)
+public class InstrumentedZooDataTest {
+
+    @Test
+    public void testLoadVertexInfo() throws FileNotFoundException {
+//        Map<String, ZooData.VertexInfo> vertexInfo = ZooData.loadVertexInfoJSON(new FileInputStream("src/main/assets/sample_node_info.json"));
+        Context c = ApplicationProvider.getApplicationContext();
+        try {
+            Map<String, ZooData.VertexInfo> vertexInfo = ZooData.loadVertexInfoJSON(c, "sample_node_info.json");
+            assertEquals("Entrance and Exit Gate", vertexInfo.get("entrance_exit_gate").name);
+            assertEquals(7, vertexInfo.size());
+        } catch (IOException e) {
+            return;
+        }
+    }
+
+}
--- app/src/test/java/com/team2/zooseeker/ZooDataTest.java
+++ app/src/test/java/com/team2/zooseeker/ZooDataTest.java
@@ -1,7 +1,9 @@
 package com.team2.zooseeker;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
+import org.jgrapht.Graph;
 import org.junit.Test;
 
 
@@ -10,16 +12,28 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.util.Map;
 
+import cse110.IdentifiedWeightedEdge;
 import cse110.ZooData;
 
 public class ZooDataTest {
 
     @Test
-    public void loadVertexInfoTest() throws FileNotFoundException {
-        Map<String, ZooData.VertexInfo> vertexInfo = ZooData.loadVertexInfoJSON(new FileInputStream(new File("src/main/assets/sample_node_info.json")));
+    public void testLoadVertexInfo() throws FileNotFoundException {
+        Map<String, ZooData.VertexInfo> vertexInfo = ZooData.loadVertexInfoJSON(new FileInputStream("src/main/assets/sample_node_info.json"));
         assertEquals("Entrance and Exit Gate", vertexInfo.get("entrance_exit_gate").name);
         assertEquals(7, vertexInfo.size());
+    }
 
+    @Test
+    public void testLoadEdgeInfo() throws FileNotFoundException {
+        Map<String, ZooData.EdgeInfo> edgeInfo = ZooData.loadEdgeInfoJSON(new FileInputStream("src/main/assets/sample_edge_info.json"));
+        assertEquals(7, edgeInfo.size());
+        assertEquals("Entrance Way", edgeInfo.get("edge-0").street);
+    }
 
+    @Test
+    public void testLoadZooGraph() throws FileNotFoundException {
+        Graph<String, IdentifiedWeightedEdge> zooGraph = ZooData.loadZooGraphJSON(new FileInputStream("src/main/assets/sample_zoo_graph.json"));
+        assertTrue(zooGraph.containsVertex("entrance_exit_gate"));
     }
 }
