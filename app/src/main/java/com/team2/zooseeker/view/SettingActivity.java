39cdd1bc280d20f46cdf1a6e451ea685791685bb
Advay Sharma <sharmaadvay0@gmail.com>
2022-05-28 16:05:16 -0700

    added delete plan button

--- app/src/main/java/com/team2/zooseeker/view/SettingActivity.java
+++ app/src/main/java/com/team2/zooseeker/view/SettingActivity.java
@@ -1,6 +1,7 @@
 package com.team2.zooseeker.view;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -8,12 +9,17 @@ import android.view.View;
 import android.widget.Button;
 
 import com.team2.zooseeker.R;
+import com.team2.zooseeker.model.ExhibitModel;
+import com.team2.zooseeker.viewModel.ExhibitListViewModel;
+
+import java.util.List;
 
 public class SettingActivity extends AppCompatActivity {
 
     private Button detailedDirectionButton;
     private Button briefDirectionButton;
     private Button summaryButton;
+    private Button deletePlanButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +29,7 @@ public class SettingActivity extends AppCompatActivity {
         detailedDirectionButton = findViewById(R.id.detailed_direction_button);
         briefDirectionButton = findViewById(R.id.brief_direction_button);
         summaryButton = findViewById(R.id.summary_button);
+        deletePlanButton = findViewById(R.id.deletePlan);
     }
 
     public void onDetailedDirectionButton(View view) {
@@ -35,6 +42,18 @@ public class SettingActivity extends AppCompatActivity {
 
     public void onSummaryButton(View view) {
         Intent intent = new Intent(this, RouteSummaryActivity.class);
+        startActivity(intent);
+    }
+
+    public void onDeletePlan(View view){
+        ExhibitListViewModel viewModel = new ViewModelProvider(this)
+                .get(ExhibitListViewModel.class);
+        List<ExhibitModel> exhibitsList = viewModel.getExhibitsListDao().getShowSelectedList(true);
+        for(ExhibitModel selectedExhibit : exhibitsList){
+            selectedExhibit.selected = false;
+            viewModel.getExhibitsListDao().update(selectedExhibit);
+        }
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
 }
